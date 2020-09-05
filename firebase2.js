22b6f5da77b571b9b03bffecb5df30d5452da8b5
1mathias123 <45027719+1mathias123@users.noreply.github.com>
2020-09-05 22:25:01 +0200

    Create firebase2.js

---
+++ firebase2.js
@@ -0,0 +1,3 @@
+const { google } = require('googleapis'); // You might need to import these someplace else.
+const { promisify } = require('util'); // You might need to import these someplace else.
+const fetch = require('node-fetch'); // You might need to import these someplace else.
